a1f60a072453c2d8e0a31ecb4f2ddfb771f83825
Eripiens <lschwalm@outlook.fr>
2019-02-28 20:12:05 +0100

    Add question finally working (almost)

--- server/server.js
+++ server/server.js
@@ -3,9 +3,10 @@ const router = require('./router');
 
 const cors = require('cors');
 const app = express();
-const fileupload = require('express-fileupload');
+const fileUpload = require('express-fileupload');
 const port = 8081;
 
 app.use(cors());
+app.use(fileUpload());
 app.use(router)
   .listen(port, () => console.log('Example app listening on port ' + port));
